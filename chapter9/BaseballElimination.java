42bec549d0e75ad6a21294044608154bed8a0d78
liuxiao8114 <liuxiao8114@163.com>
2017-11-16 00:34:58 +0800

    update baseball

--- chapter9/BaseballElimination.java
+++ chapter9/BaseballElimination.java
@@ -1,19 +1,25 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination { 
   private final int V;
   private final ST<String, Team> teams;
   private final String[] names;
-  private char[] isEliminated;
+  private final int[] isEliminated;
+  private final Bag<String>[] eliminateTeams;
   
-  private int[][] g;
+  private final int[][] g;
   
   private class Team {
-    String name;
-    int index, win, lose, remain;
+    int index, wins, losses, remain;
     
-    public Team(int index, String name, int win, int lose, int remain) {
-      this.index = index; this.name = name; this win = win; this.lose = lose; this.remain = remain;
+    public Team(int index, int wins, int losses, int remain) {
+      this.index = index; this.wins = wins; this.losses = losses; this.remain = remain;
     }
   }
   
@@ -21,15 +27,24 @@ public class BaseballElimination {
   public BaseballElimination(String filename) {
     In in = new In(filename);
     V = Integer.parseInt(in.readLine());
+    g = new int[V][V];
+    teams = new ST<String, Team>();
+    names = new String[V]; 
+    isEliminated = new int[V];
+    eliminateTeams = (Bag<String>[])new Bag[V];
     
     String[] teamsRaw = in.readAllLines();
     
     for(int i = 0; i < teamsRaw.length; i++) {
       //0: teamName, 1: win, 2: lose, 3: remain, 4~length-1: division 
-      String rawData = teamsRaw[i].split("\\s+");
-      int k = 4;
+      String[] rawDatas = teamsRaw[i].split("\\s+");
+      int n = 0;
+      if(rawDatas[n].equals("")) n++;
+      teams.put(rawDatas[n], new Team(i, Integer.parseInt(rawDatas[n + 1]), Integer.parseInt(rawDatas[n + 2]), Integer.parseInt(rawDatas[n + 3])));
+      names[i] = rawDatas[n];
+      int k = n + 4;
       for(int j = 0; j < teamsRaw.length; j++) {
-        g[i][j] = rawData[k++];
+        g[i][j] = Integer.parseInt(rawDatas[k++]);
       }
     }
   }
@@ -41,94 +56,121 @@ public class BaseballElimination {
   
   // all teams                     
   public Iterable<String> teams() {
-    
+    return teams.keys();
   }   
   
   // number of wins for given team                            
   public int wins(String team) {
     validateArgument(team);
+    return teams.get(team).wins;
   }      
   
   // number of losses for given team               
   public int losses(String team) {
     validateArgument(team);
+    return teams.get(team).losses;
   } 
   
    // number of remaining games for given team                  
   public int remaining(String team) {
     validateArgument(team);
+    return teams.get(team).remain;
   }           
   
   // number of remaining games between team1 and team2 
   public int against(String team1, String team2) {
     validateArgument(team1);
     validateArgument(team2);
+    
+    return g[teams.get(team1).index][teams.get(team2).index];
   }    
   
   // is given team eliminated?
   public boolean isEliminated(String team) {
     validateArgument(team);
-    char c = this.isEliminated[names.get(team)];
-    if(c == null) {
-      c = eliminate(team);
+    int x = isEliminated[teams.get(team).index];
+    if(x == 0) {
+      return eliminate(team);
     } 
     
-    return c == '1';
+    return x == 1;
   }
   
   // subset R of teams that eliminates given team; null if not eliminated        
   public Iterable<String> certificateOfElimination(String team) {
     validateArgument(team);
+    int v = teams.get(team).index;
+    if(isEliminated[v] == 0)
+      eliminate(team);
+    
+    return eliminateTeams[v];
   }
   
   private boolean eliminate(String team) {
-    int vertices = V - 1 + V * (V - 1) / 2 + 2;
+    int vertices = V - 1 + (V - 1) * (V - 2) / 2 + 2;
     FlowNetwork G = new FlowNetwork(vertices);
     Team tarTeam = teams.get(team);
-    int curIndex = tarTeam.index;
-    
+    int tarIndex = tarTeam.index;
+    int sum = 0, gameIndex = 1, verticesIndex = vertices - V;
+
     // build network
-    for(int i = 0, VIndex = V + 1; i < V; i++) {
-      if(i == curIndex) continue;
+    for(int i = 0; i < V; i++) {
+      if(i == tarIndex) continue;
       Team curTeam = teams.get(names[i]);
-      if(tarTeam.win + tarTeam.remain - curTeam.win < 0) {
-        this.isEliminated[curIndex] = '1';
-        return false;
+      if(tarTeam.wins + tarTeam.remain - curTeam.wins < 0) {
+        isEliminated[tarIndex] = 1;
+        eliminateTeams[tarIndex] = new Bag<String>();
+        eliminateTeams[tarIndex].add(names[i]);
+        return true;
       }
-      
+
       // add every games FlowEdge
-      for(int j = i + 1, gameIndex = 1; j < V; j++) {
-          if(j == curIndex) continue;
-          G.addEdge(new FlowEdge(0, gameIndex, g[i][j]));
+      for(int j = i + 1; j < V; j++) {
+        if(j == tarIndex) continue;
+        G.addEdge(new FlowEdge(0, gameIndex, g[i][j]));
+        sum += g[i][j];
+        // add vertices, when (vertices - v) is the start index at team
+        G.addEdge(new FlowEdge(gameIndex, i + vertices - V, Double.POSITIVE_INFINITY));
+        G.addEdge(new FlowEdge(gameIndex, j + vertices - V, Double.POSITIVE_INFINITY));
+        gameIndex++;
       }
       
-      // add vertices and compareTo  edge 
-      G.addEdge(new FlowEdge(gameIndex, VIndex, 1);
+      G.addEdge(new FlowEdge(verticesIndex++, vertices - 1, tarTeam.wins + tarTeam.remain - curTeam.wins));
     }
+
+//    System.out.println("at the end of game loop, gameIndex = vertices - V");
+//    System.out.println("in this case vertices=" + vertices + ", V=" + V + ", and gameIndex is: " + gameIndex);
     
-    FordFulkerson ff = new FordFulkerson(G);
+    FordFulkerson ff = new FordFulkerson(G, 0, vertices - 1);
     
-    ff.value()
+    if(ff.value() == sum) {
+      isEliminated[tarIndex] = 2;
+      eliminateTeams[tarIndex] = null;
+      return false;
+    } else if(ff.value() < sum){
+      isEliminated[tarIndex] = 1;
+      eliminateTeams[tarIndex] = new Bag<String>();
+      
+      for(int i = gameIndex; i < vertices - 1; i++) {
+        if(ff.inCut(i) && i - gameIndex != tarIndex) 
+          eliminateTeams[tarIndex].add(names[i - gameIndex]);
+      }
+      
+      return true;
+    } else {
+      throw new IllegalArgumentException("this is impossible! check the sum calculation");
+    }
   }
 
   private void validateArgument(String s) {
-    if(s == null) throw new IllegalArgumentException();
+    if(s == null || !teams.contains(s)) throw new IllegalArgumentException();
   }
   
   public static void main(String[] args) {
     BaseballElimination division = new BaseballElimination(args[0]);
-    for (String team : division.teams()) {
-        if (division.isEliminated(team)) {
-            StdOut.print(team + " is eliminated by the subset R = { ");
-            for (String t : division.certificateOfElimination(team)) {
-                StdOut.print(t + " ");
-            }
-            StdOut.println("}");
-        }
-        else {
-            StdOut.println(team + " is not eliminated");
-        }
-    }
+
+    
+    division.eliminate("Ghaddafi");
   }
 }
 
