078cfc6ab3eae5d289b4ded8293e703676f1790f
liuxiao <liuxiao8114@163.com>
2017-11-23 13:06:15 +0800

    update

--- chapter10/BoggleSolver.java
+++ chapter10/BoggleSolver.java
@@ -1,18 +1,34 @@
 import edu.princeton.cs.algs4.Queue;
 
 public class BoggleSolver {
+  private final int R = 256;
+  private TrieST st;
   // Initializes the data structure using the given array of strings as the dictionary.
   // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
   public BoggleSolver(String[] dictionary) {
-
+    st = new TrieST();
+    for(int i = 0; i < dictionary.length; i++) {
+      st.add(dictionary[i]);
+      /*
+      int length = dictionary[i].length();
+      for(int j = 0; j < length; j++) {
+        dictionary[i].charAt(j);
+      }
+      */
+    }
   }
 
   private void dfs(int i, int j, BoggleBoard board,
-    int[][] marked, StringBuilder prefix, Iterable<String> result) {
+    int[][] marked, StringBuilder prefix, Iterable<String> result, int scores) {
 
     // check
     prefix.append(board.getLetter(i, j));
-    prefix.
+    String s = prefix.toString();
+
+    if(st.contains(s)) {
+      scores += scoreOf(s);
+      result;
+    }
 
     // build adjacent dice
     Queue<int[]> q = new Queue<int[]>();
@@ -42,10 +58,10 @@ public class BoggleSolver {
   public Iterable<String> getAllValidWords(BoggleBoard board) {
     int row = board.rows(), col = board.cols();
     Queue<String> result = new Queue<String>();
-
+    int scores = 0;
     for(int i = 0; i < row; i++) {
       for(int j = 0; j < col; j++)
-        dfs(i, j, board, new boolean[row][col], new StringBuilder(), result);
+        dfs(i, j, board, new boolean[row][col], new StringBuilder(), result, scores);
     }
   }
 
