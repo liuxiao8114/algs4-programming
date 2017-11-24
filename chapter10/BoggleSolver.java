3761228a5d7dfb40426e74979f89a136a27c89fe
liuxiao <liuxiao8114@163.com>
2017-11-24 16:57:40 +0800

    update BoggleSolver.java & rewrite TrieST.java

--- chapter10/BoggleSolver.java
+++ chapter10/BoggleSolver.java
@@ -1,14 +1,19 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.TrieST;
 
 public class BoggleSolver {
-  private final int R = 256;
-  private TrieST st;
+//  private final int R = 256;
+
+  private TrieST<Boolean> st;
   // Initializes the data structure using the given array of strings as the dictionary.
   // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
   public BoggleSolver(String[] dictionary) {
-    st = new TrieST();
+    st = new TrieST<Boolean>();
     for(int i = 0; i < dictionary.length; i++) {
-      st.add(dictionary[i]);
+      st.put(dictionary[i], true);
       /*
       int length = dictionary[i].length();
       for(int j = 0; j < length; j++) {
@@ -18,56 +23,114 @@ public class BoggleSolver {
     }
   }
 
-  private void dfs(int i, int j, BoggleBoard board,
-    int[][] marked, StringBuilder prefix, Iterable<String> result, int scores) {
+  private class Node {
+    int row, col;
+    Node(int row, int col) {
+      this.row = row;
+      this.col = col;
+    }
+  }
 
+  private void dfs(int i, int j, BoggleBoard board, boolean[] marked,
+    StringBuilder prefix, Bag<String> result) {
+    int row = board.rows(), col = board.cols();
     // check
     prefix.append(board.getLetter(i, j));
     String s = prefix.toString();
 
-    if(st.contains(s)) {
-      scores += scoreOf(s);
-      result;
+    marked[i * col + j] = true;
+
+    if(s.equals("YO")) {
+    	System.out.println();
+    }
+
+    if(!st.keysWithPrefix(s).iterator().hasNext()) return;
+    if(st.contains(s) && st.get(s) && s.length() > 2) {
+    	result.add(s);
+    	st.put(s, false);
     }
 
     // build adjacent dice
-    Queue<int[]> q = new Queue<int[]>();
+    Queue<Node> q = new Queue<Node>();
+
     if(i + 1 < row) {
-      if(!marked[i + 1][j]) q.enquque(board.getLetter(i + 1, j));
-      if(j + 1 < col && !marked[i + 1][j + 1]) q.enquque(board.getLetter(i + 1, j + 1));
-      if(j > 0  && !marked[i + 1][j - 1]) q.enquque(board.getLetter(i + 1, j - 1));
+      if(!marked[(i + 1) * col + j]) {
+        q.enqueue(new Node(i + 1, j));
+      }
+      if(j + 1 < col && !marked[(i + 1) * col + j + 1]) {
+        q.enqueue(new Node(i + 1, j + 1));
+      }
+      if(j > 0  && !marked[(i + 1) * col + j - 1]) {
+        q.enqueue(new Node(i + 1, j - 1));
+      }
     }
 
     if(i > 0) {
-      if(!marked[i - 1][j]) q.enquque(board.getLetter(i - 1, j));
-      if(j + 1 < row && !marked[i - 1][j + 1]) q.enquque(board.getLetter(i - 1, j + 1));
-      if(j > 0 && !marked[i - 1][j - 1]) q.enquque(board.getLetter(i - 1, j - 1));
+      if(!marked[(i - 1) * col + j]) {
+        q.enqueue(new Node(i - 1, j));
+      }
+      if(j + 1 < row && !marked[(i - 1) * col + j + 1]) {
+        q.enqueue(new Node(i - 1, j + 1));
+      }
+      if(j > 0 && !marked[(i - 1) * col + j - 1]) {
+        q.enqueue(new Node(i - 1, j - 1));
+      }
     }
 
-    if(j + 1 < row && !marked[i][j + 1]) q.enquque(board.getLetter(i, j + 1));
-    if(j > 0 && !marked[i][j - 1]) q.enquque(board.getLetter(i, j - 1));
+    if(j + 1 < row && !marked[i * col + j + 1]) {
+      q.enqueue(new Node(i, j + 1));
+    }
+
+    if(j > 0 && !marked[i * col + j - 1]) {
+      q.enqueue(new Node(i, j - 1));
+    }
 
     // Recursive
     while(!q.isEmpty()) {
-      char c = q.dequeue();
-      dfs(i, j, board, marked, prefix.append(), result);
+      Node n = q.dequeue();
+      dfs(n.row, n.col, board, marked.clone(),
+        new StringBuilder(prefix), result);
     }
   }
 
   // Returns the set of all valid words in the given Boggle board, as an Iterable.
   public Iterable<String> getAllValidWords(BoggleBoard board) {
     int row = board.rows(), col = board.cols();
-    Queue<String> result = new Queue<String>();
-    int scores = 0;
+    Bag<String> result = new Bag<String>();
     for(int i = 0; i < row; i++) {
-      for(int j = 0; j < col; j++)
-        dfs(i, j, board, new boolean[row][col], new StringBuilder(), result, scores);
+      for(int j = 0; j < col; j++) {
+    	if(i == 1 && j == 2) {
+    		System.out.println("this is : " + board.getLetter(i, j));
+    	}
+        dfs(i, j, board, new boolean[row * col], new StringBuilder(), result);
+      }
     }
+    return result;
   }
 
   // Returns the score of the given word if it is in the dictionary, zero otherwise.
   // (You can assume the word contains only the uppercase letters A through Z.)
   public int scoreOf(String word) {
-
+    int length = word.length();
+    if(length <= 2) return 0;
+    if(length <= 4) return 1;
+    if(length == 5) return 2;
+    if(length == 6) return 3;
+    if(length == 7) return 5;
+    if(length >= 8) return 11;
+    else return 0;
   }
+
+  public static void main(String[] args) {
+    In in = new In(args[0]);
+    String[] dictionary = in.readAllStrings();
+    BoggleSolver solver = new BoggleSolver(dictionary);
+    BoggleBoard board = new BoggleBoard(args[1]);
+    int score = 0;
+    for (String word : solver.getAllValidWords(board)) {
+        StdOut.println(word);
+        score += solver.scoreOf(word);
+    }
+    StdOut.println("Score = " + score);
+}
 }
---
+++ chapter10/memo.md
@@ -0,0 +1,20 @@
+t e a s
+h r e e
+e i l l
+m p k c
+
+1.dfs的实现:
+单次检索去重,再次检索需reset重复判断flag
+如: the和hree
+t
+h r e e
+e
+
+有序,如ear和are
+e a
+r
+
+[false, true, true, true],
+[true, true, true, true],
+[true, true, true, true],
+[true, true, true, false]
