5ec4e63c963ba702218963b80f62d7cd070f88dd
liuxiao <liuxiao8114@163.com>
2017-12-05 16:55:17 +0800

    update Circle.java in week7, init final hw in week11

---
+++ week11/Huffman.java
@@ -0,0 +1,107 @@
+public class Huffman {
+  private static final int R = 256;
+
+  private static class Node implements Comparble<Node> {
+    private final char ch;
+    private final int freq;
+    private final Node left, right;
+
+    Node(char ch, int freq, Node left, Node right) {
+      this.ch = ch;
+      this.freq = freq;
+      this.left = left;
+      this.right = right;
+    }
+
+    private boolean isLeaf() {
+      return (this.left == null && this.right == null)
+    }
+
+    public int compareTo(Node that) {
+      return this.freq - that.freq;
+    }
+  }
+
+  public static void compress() {
+    String s = BinaryStdIn.readString();
+    char[] input = s.toCharArray();
+
+    int[] freq = new int[R];
+    for(int i = 0; i < input.length; i++)
+      freq[input[i]]++;
+
+    Node root = buildTrie(freq);
+
+    String[] st = new String[R];
+    buildCode(st, root, "");
+
+    writeTrie(root);
+
+    BinaryStdOut.write(input.length);
+
+    for(int i = 0; i < input.length; i++) {
+      String code = input[i];
+      for(int j = 0; j < code.length(); j++) {
+        if(code.charAt(j) == '0') {
+          BinaryStdOut.write(false);
+        } else if(code.charAt(j) == '1') {
+          BinaryStdOut.write(true);
+        } else {
+          throw new IllegalStateException("Illegal state");
+        }
+      }
+    }
+
+    BinaryStdOut.close();
+  }
+
+  public static void expand() {
+    Node root = readTrie();
+
+    int length = BinaryStdIn.readInt();
+
+
+  }
+
+  private static void writeTrie(Node x) {
+    if(x.isLeaf()) {
+      BinaryStdOut.write(true);
+      BinaryStdOut.write(x.ch, 8);
+    } else {
+      BinaryStdOut.write(false);
+      writeTrie(x.left);
+      writeTrie(x.right);
+    }
+  }
+
+  private static Node buildTrie(int[] freq) {
+    MinPQ<Node> pq = new MinPQ<Node>();
+    for(char i = 0; i < R; i++) {
+      if(freq[i] > 0)
+        pq.insert(new Node(i, frqe[i], null, null));
+    }
+
+    // special case in case there is only one character with a nonzero frequency
+    if(pq.size() == 1) {
+      if(freq['\0'] == 0) pq.insert(new Node('\0', 0, null, null));
+      else pq.insert(new Node('\1', 0, null, null));
+    }
+
+    while(pq.size() > 1) {
+      Node x = pq.delMin();
+      Node y = pq.delMin();
+      pq.insert(new Node('\0', x.freq + y.freq, x, y));
+    }
+
+    return pq.delMin();
+  }
+
+  private static void buildCode(String[] st, Node x, String s) {
+    if(!x.isLeaf()) {
+      buildCode(st, x.left, s + '0');
+      buildCode(st, x.right, s + '1');
+    } else {
+      st[x.ch] = s;
+    }
+  }
+}
--- week7/Circle.java
+++ week7/Circle.java
@@ -1,33 +1,62 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Circle {
   private boolean[] marked;
+  private int[] edgeTo;
+  private boolean isCircle;
+  private boolean[] circleVerties;
 
-  public Cycle(Graph G) {
+  public Circle(Graph G) {
     if (hasSelfLoop(G)) return;
     if (hasParallelEdges(G)) return;
     marked = new boolean[G.V()];
     edgeTo = new int[G.V()];
-    for (int v = 0; v < G.V(); v++)
+    isCircle = false;
+    circleVerties = new boolean[G.V()];
+
+    for (int v = 0; v < G.V(); v++) {
         if (!marked[v])
             dfs(G, -1, v);
+    }
+
+  }
+
+  private boolean hasSelfLoop(Graph g) {
+    for(int v = 0; v < g.V(); v++) {
+      for(int w : g.adj(v)) {
+        if(w == v) {
+          return true;
+        }
+      }
+    }
+
+  	return false;
+  }
+
+  private boolean hasParallelEdges(Graph g) {
+	   return false;
   }
 
   private void dfs(Graph g, int v, int s) {
-    marked[v] = true;
-    for(int w : g.adj(v)) {
+	  marked[s] = true;
+    for(int w : g.adj(s)) {
       if(!marked[w]) {
-        dfs(g, w, s);
-      } else {
-
+    	  edgeTo[w] = s;
+        dfs(g, s, w);
+      } else if(w != v) {
+    	  isCircle = true;
+    	  circleVerties[w] = true;
       }
     }
   }
 
   public boolean isCircle() {
-
+	  return isCircle;
   }
 
-  public boolean isCirCle(int v) {
-
+  public boolean isCircle(int v) {
+	  return circleVerties[v];
   }
 
   public static void main(String[] args) {
